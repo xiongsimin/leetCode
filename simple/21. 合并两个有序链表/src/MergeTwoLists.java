8aa9d4b2f1ff7e5802db94e60a6a29532c334c36
xiongsm <xiongsm@yonyou.com>
2021-03-02 12:22:01 +0800

    21. 合并两个有序链表

--- simple/21. 合并两个有序链表/src/MergeTwoLists.java
+++ simple/21. 合并两个有序链表/src/MergeTwoLists.java
@@ -26,15 +26,59 @@
  */
 public class MergeTwoLists {
     public ListNode21 mergeTwoLists(ListNode21 l1, ListNode21 l2) {
-        //TODO
-        return new ListNode21();
+        //先确定链表头
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode21 head = l1.val < l2.val ? l1 : l2;
+        ListNode21 current = head;
+        ListNode21 nextA = head.next;
+        ListNode21 nextB = head == l1 ? l2 : l1;
+        //依次按升序将节点串起来
+        while (nextA != null && nextB != null) {
+            if (nextA.val < nextB.val) {
+                current.next = nextA;
+                current = nextA;
+                nextA = nextA.next;
+            } else {
+                current.next = nextB;
+                current = nextB;
+                nextB = nextB.next;
+            }
+        }
+        //当一个链表到末尾时，仅需将其尾部与另一个链表下个元素连接起来就可以
+        if (nextA == null) {
+            current.next = nextB;
+        } else {
+            current.next = nextA;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        MergeTwoLists mergeTwoLists = new MergeTwoLists();
+        ListNode21 one3 = new ListNode21(4);
+        ListNode21 one2 = new ListNode21(2, one3);
+        ListNode21 one1 = new ListNode21(1, one2);
+
+        ListNode21 two3 = new ListNode21(4);
+        ListNode21 two2 = new ListNode21(3, two3);
+        ListNode21 two1 = new ListNode21(1, two2);
+        ListNode21 result = mergeTwoLists.mergeTwoLists(one1, two1);
+        ListNode21 head = result;
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
 }
 
 
 class ListNode21 {
     int val;
-    ListNode next;
+    ListNode21 next;
 
     ListNode21() {
     }
@@ -43,7 +87,7 @@ class ListNode21 {
         this.val = val;
     }
 
-    ListNode21(int val, ListNode next) {
+    ListNode21(int val, ListNode21 next) {
         this.val = val;
         this.next = next;
     }
