dba2a3b2ca427a3631e16bf489fa267bd50300e5
xiongsm <xiongsm@yonyou.com>
2021-03-22 13:12:04 +0800

    100. 相同的树

---
+++ simple/100. 相同的树/src/SameTree.java
@@ -0,0 +1,58 @@
+/**
+ * @author xiongsm
+ * @Date 2021-03-22
+ * @Description 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * @示例 见附件
+ * @提示： 两棵树上的节点数目都在范围 [0, 100] 内
+ * -10^4 <= Node.val <= 10^4
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        SameTree sameTree = new SameTree();
+        System.out.println(sameTree.isSameTree(p, q));
+    }
+}
+
+/*  Definition for a binary tree node.*/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
