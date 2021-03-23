35698ad3f4c72bdf40f4db21ad56ebfa66fcb30f
xiongsm <xiongsm@yonyou.com>
2021-03-23 13:54:30 +0800

    K个一组翻转链表--临时

---
+++ common/src/ListNode.java
@@ -0,0 +1,20 @@
+/**
+ * @author xiongsm
+ * @Date 2021-03-23
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
--- difficult/23. 合并K个升序链表/src/MergeKLists.java
+++ difficult/23. 合并K个升序链表/src/MergeKLists.java
@@ -141,23 +141,3 @@ public class MergeKLists {
         }
     }
 }
-
-/**
- * Definition for singly-linked list.
- */
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
