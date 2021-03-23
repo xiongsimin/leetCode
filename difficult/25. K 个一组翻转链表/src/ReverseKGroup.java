0b4c6e51a6f27ebaced34d2625f0efc335c9c94e
aries <2248950919@qq.com>
2021-03-23 23:37:02 +0800

    25. K 个一组翻转链表

--- difficult/25. K 个一组翻转链表/src/ReverseKGroup.java
+++ difficult/25. K 个一组翻转链表/src/ReverseKGroup.java
@@ -37,10 +37,43 @@
  */
 public class ReverseKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-        //todo 每次循环前检查当前组是否够k个元素
-        //每组做链表反转操作
-        //每组的首尾需要连接起来
-        return null;
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
     }
 }
 
