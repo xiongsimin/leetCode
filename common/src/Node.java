a2cce69c2ddfd95a7ef6c0d8e798693974d2e64b
aries <2248950919@qq.com>
2021-07-18 12:07:27 +0800

    138. 复制带随机指针的链表（深拷贝）

---
+++ common/src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * @Author aries
+ * @Data 2021-07-18
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
