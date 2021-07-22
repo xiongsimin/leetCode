fa80b8026982db7f694fc0e6968c78d38d20a816
aries <2248950919@qq.com>
2021-07-22 21:52:59 +0800

    224.基本计算器

--- difficult/224. 基本计算器/src/Calculate.java
+++ difficult/224. 基本计算器/src/Calculate.java
@@ -33,101 +33,47 @@ import java.util.Stack;
  */
 public class Calculate {
     public int calculate(String s) {
-        //操作数栈
-        Stack<Integer> dataStack = new Stack<>();
-        //运算符栈
-        Stack<Character> symbolStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
+        Stack<Integer> stack=new Stack<>();
+        int res=0;
+        int num=0;
+        //符号
+        int sign=1;
+        for(int i=0;i<s.length();i++){
+            if(' '==s.charAt(i)){
                 continue;
             }
-            //操作数，考虑特殊情况（负数）
-            else if ('0' <= s.charAt(i) && '9' >= s.charAt(i)) {
-                String data = String.valueOf(s.charAt(i));
-                while (i < s.length() - 1 && '0' <= s.charAt(i + 1) && '9' >= s.charAt(i + 1)) {
-                    data += s.charAt(i + 1);
-                    i++;
-                }
-                dataStack.push(Integer.parseInt(data));
-                if (!symbolStack.isEmpty() && (symbolStack.peek() == '-' || symbolStack.peek() == '+')) {
-                    //取数字栈顶两个数字做运算
-                    int data1 = dataStack.pop();
-                    int data2 = dataStack.pop();
-                    if ('+' == symbolStack.peek()) {
-                        //将结果重新入栈
-                        dataStack.push(data2 + data1);
-                    } else {
-                        dataStack.push(data2 - data1);
-                    }
-                    symbolStack.pop();
+            else if('0'<=s.charAt(i)&&s.charAt(i)<='9'){
+                num=(num*10+s.charAt(i)-'0');
+                if(i<s.length()-1&&s.charAt(i+1)>='0'&&s.charAt(i+1)<='9'){
+                    continue;
                 }
             }
-            //运算符
-            else if (')' == s.charAt(i)) {
-                while (symbolStack.peek() == '('||(!symbolStack.isEmpty()&&(symbolStack.peek() == '-' || symbolStack.peek() == '+'))) {
-                    if('('==s.charAt(i)){
-                        //“(”出栈
-                        symbolStack.pop();
-                        break;
-                    }
-                    char symbol = symbolStack.pop();
-                    //取数字栈顶两个数字做运算
-                    int data1 = dataStack.pop();
-                    int data2 = dataStack.pop();
-                    if ('+' == symbol) {
-                        //将结果重新入栈
-                        dataStack.push(data2 + data1);
-                    } else {
-                        dataStack.push(data2 - data1);
-                    }
-                }
-
-            } else {
-                if('-'==s.charAt(i)){
-                    //往前读取，遇到的第一个有效符号为数字，则当做减号处理，否则当做负号处理
-                    int j = i - 1;
-                    boolean isNegativeSymbol = true;
-                    while (j >= 0) {
-                        if (' ' == s.charAt(j)) {
-                            j--;
-                            continue;
-                        }
-                        //必是负号
-                        else if ('(' == s.charAt(j)) {
-                            break;
-                        }
-                        //必是减号
-                        else if (')' == s.charAt(j) || ('0' <= s.charAt(j) && '9' >= s.charAt(j))) {
-                            isNegativeSymbol = false;
-                            break;
-                        }
-                        //循环结束，前面没有了，则也是负号
-                    }
-                    if (isNegativeSymbol) {
-                        dataStack.push(0);
-                    }
-                }
-                symbolStack.push(s.charAt(i));
+            else if('('==s.charAt(i)){
+                stack.push(res);
+                stack.push(sign);
+                res=0;
+                num=0;
+                sign=1;
             }
+            else if(')'==s.charAt(i)){
+                num=res;
+                sign=stack.pop();
+                res=stack.pop();
+            }else{
+                if('+'==s.charAt(i)){
+                    sign=1;
+                }else{
+                    sign=-1;
+                }
+                num=0;
+            }
+            res+=sign*num;
         }
-        //做栈中剩余运算
-        while (!symbolStack.isEmpty()) {
-            char symbol = symbolStack.pop();
-            //取数字栈顶两个数字做运算
-            int data1 = dataStack.pop();
-            int data2 = dataStack.pop();
-            if ('+' == symbol) {
-                //将结果重新入栈
-                dataStack.push(data2 + data1);
-            } else {
-                dataStack.push(data2 - data1);
-            }
-        }
-        return dataStack.pop();
+        return res;
     }
 
     public static void main(String[] args) {
         Calculate calculate = new Calculate();
-        System.out.println(calculate.calculate("1-1"));
+        System.out.println(calculate.calculate("2147483647"));
     }
 }
