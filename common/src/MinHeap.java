a890703bb52be127647f70930cd826e459391389
aries <2248950919@qq.com>
2021-07-22 22:55:34 +0800

    //todo 实现最小堆

---
+++ common/src/MinHeap.java
@@ -0,0 +1,35 @@
+/**
+ * @Author aries
+ * @Data 2021-07-22
+ * @Description 最小堆
+ */
+public class MinHeap {
+
+    TreeNode root=null;
+
+
+    public MinHeap() {
+    }
+
+    //给定已知数组，构建最小堆
+    static void buildMinHeap(int[] nums){
+        //todo
+
+    }
+
+    //往最小堆中添加节点
+    boolean offer(TreeNode node){
+        //todo
+        return true;
+    }
+
+    //删除最小堆堆顶
+    TreeNode poll(){
+        return null;
+    }
+
+    //获堆顶
+    TreeNode peek(){
+        return root;
+    }
+}
