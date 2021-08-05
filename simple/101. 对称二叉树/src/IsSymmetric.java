fc6afe46963c181117cd5fcd69258435e08e8421
aries <2248950919@qq.com>
2021-08-05 22:52:18 +0800

    对称的二叉树

---
+++ simple/101. 对称二叉树/src/IsSymmetric.java
@@ -0,0 +1,44 @@
+/**
+ * @Author aries
+ * @Data 2021-08-05
+ * @Description 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *  
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *  
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(q.right, p.left);
+    }
+
+}
