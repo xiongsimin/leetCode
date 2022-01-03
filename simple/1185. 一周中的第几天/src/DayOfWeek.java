b3abe294d0d3f8e9a380939dbb408c3d8f1db473
xiongsm <xiongsm@yonyou.com>
2022-01-03 17:34:48 +0800

    441. 排列硬币

---
+++ simple/1185. 一周中的第几天/src/DayOfWeek.java
@@ -0,0 +1,59 @@
+/**
+ * @author xiongsm
+ * @Date 2022-01-03
+ * @Description 给你一个日期，请你设计一个算法来判断它是对应一周中的哪一天。
+ * <p>
+ * 输入为三个整数：day、month 和 year，分别表示日、月、年。
+ * <p>
+ * 您返回的结果必须是这几个值中的一个 {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：day = 31, month = 8, year = 2019
+ * 输出："Saturday"
+ * 示例 2：
+ * <p>
+ * 输入：day = 18, month = 7, year = 1999
+ * 输出："Sunday"
+ * 示例 3：
+ * <p>
+ * 输入：day = 15, month = 8, year = 1993
+ * 输出："Sunday"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 给出的日期一定是在 1971 到 2100 年之间的有效日期。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/day-of-the-week
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DayOfWeek {
+    /**
+     * 1970年12月31号是周四，故计算截止到所求日期的天数D，(D+3)%7即为week[7]数组下标
+     *
+     * @param day
+     * @param month
+     * @param year
+     * @return
+     */
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        /* 输入年份之前的年份的天数贡献 */
+        int days = 365 * (year - 1971) + (year - 1969) / 4;
+        /* 输入年份中，输入月份之前的月份的天数贡献 */
+        for (int i = 0; i < month - 1; ++i) {
+            days += monthDays[i];
+        }
+        if ((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) && month >= 3) {
+            days += 1;
+        }
+        /* 输入月份中的天数贡献 */
+        days += day;
+        return week[(days + 3) % 7];
+    }
+}
