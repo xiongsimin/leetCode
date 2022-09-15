eb28c44ee36738a269fb6ef72883c92b9b7731bc
xiongsimin <2248950919@qq.com>
2022-09-15 23:27:00 +0800

    58. 最后一个单词的长度

---
+++ simple/58. 最后一个单词的长度/src/LengthOfLastWord.java
@@ -0,0 +1,53 @@
+/**
+ * ((No javadoc))
+ *
+ * @author xiongsimin
+ * @date 2022/9/15
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+ *
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "Hello World"
+ * 输出：5
+ * 解释：最后一个单词是“World”，长度为5。
+ * 示例 2：
+ *
+ * 输入：s = "   fly me   to   the moon  "
+ * 输出：4
+ * 解释：最后一个单词是“moon”，长度为4。
+ * 示例 3：
+ *
+ * 输入：s = "luffy is still joyboy"
+ * 输出：6
+ * 解释：最后一个单词是长度为6的“joyboy”。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 104
+ * s 仅有英文字母和空格 ' ' 组成
+ * s 中至少存在一个单词
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/length-of-last-word
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int len = s.length();
+        int ans = 0;
+        int i = len - 1;
+        while (s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            ans++;
+            i--;
+        }
+        return ans;
+    }
+}
