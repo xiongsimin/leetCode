622e7f3bf2f2b7542beb20ac1240104bcf68d29b
xiongsimin <2248950919@qq.com>
2022-10-04 10:55:27 +0800

    124. 二叉树中的最大路径和

---
+++ difficult/124. 二叉树中的最大路径和/src/MaxPathSum.java
@@ -0,0 +1,54 @@
+/**
+ * ((No javadoc))
+ *
+ * @author xiongsimin
+ * @date 2022/10/4
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 示例 2：
+ *
+ *
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目范围是 [1, 3 * 104]
+ * -1000 <= Node.val <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/binary-tree-maximum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaxPathSum {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        nodeContribute(root);
+        return maxSum;
+    }
+
+    int nodeContribute(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftContribute = Math.max(0, nodeContribute(node.left));
+        int rightContribute = Math.max(0, nodeContribute(node.right));
+        maxSum = Math.max(maxSum, leftContribute + rightContribute + node.val);
+        return Math.max(leftContribute, rightContribute) + node.val;
+    }
+}
