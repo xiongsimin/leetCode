e0d0aff94582eca6cfb68ba1ba81c854e73f3eba
xiongsimin <2248950919@qq.com>
2023-08-17 12:53:08 +0800

    1444. 切披萨的方案数

---
+++ difficult/1444. 切披萨的方案数/src/CutPizzaWays.java
@@ -0,0 +1,15 @@
+/**
+ * ((No javadoc))
+ *
+ * @author xiongsimin
+ * @date 2023/8/17
+ */
+public class CutPizzaWays {
+    public int ways(String[] pizza, int k) {
+        if (pizza.length == 0) {
+            return 0;
+        }
+        // todo
+        return 0;
+    }
+}
